81d06e9996f6357cd21f2e1a9848dcef77e4a6ef
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-03-02 17:02:18 +0100

    CRATE; new repo and install react

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/homepage';
+
+
+function App() {
+
+
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+    <Route></Route>
+    </Routes>
+
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
